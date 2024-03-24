35013b04fd153278f59963fb17befa218a7a45ec
hehehai <riverhohai@gmail.com>
2024-03-24 21:05:36 +0800

    fix: ci not found date-fns-z

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,6 @@ import {
   differenceInMinutes,
   format,
 } from "date-fns";
-import { utcToZonedTime } from "date-fns-tz";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -21,12 +20,14 @@ export const randomInt = (start: number, end: number) => {
 };
 
 export const formatPosDate = (val: Date) => {
-  const now = utcToZonedTime(new Date(), "Asia/Shanghai");
+  const now = new Date();
+  const nowShanghai = convertToShanghaiTime(now);
+  const valShanghai = convertToShanghaiTime(val);
 
-  const readStr = format(val, "MM/dd HH:mm");
+  const readStr = format(valShanghai, "yyyy/MM/dd HH:mm");
 
   // 如果时间在 5 分钟内，显示 刚刚
-  const minutesDiff = differenceInMinutes(now, val);
+  const minutesDiff = differenceInMinutes(nowShanghai, valShanghai);
   if (minutesDiff <= 5) {
     return `刚刚 | ${readStr}`;
   }
@@ -37,17 +38,26 @@ export const formatPosDate = (val: Date) => {
   }
 
   // 如果时间在 12 小时内，显示 xx 小时前
-  const hoursDiff = differenceInHours(now, val);
+  const hoursDiff = differenceInHours(nowShanghai, valShanghai);
   if (hoursDiff <= 24) {
     return `${hoursDiff} 小时前 | ${readStr}`;
   }
 
   // 如果时间在 5 天内，显示 xx 天前
-  const daysDiff = differenceInDays(now, val);
+  const daysDiff = differenceInDays(nowShanghai, valShanghai);
   if (daysDiff <= 5) {
     return `${daysDiff} 天前 | ${readStr}`;
   }
 
   // 如果时间大于 5 天，显示时间
-  return format(val, "MM/dd HH:mm");
+  return format(valShanghai, "MM/dd HH:mm");
+};
+
+const convertToShanghaiTime = (date: Date): Date => {
+  const shanghaiOffset = 8 * 60; // Shanghai is UTC+8
+  const utcOffset = date.getTimezoneOffset();
+  const convertedDate = new Date(
+    date.getTime() + (utcOffset + shanghaiOffset) * 60 * 1000,
+  );
+  return convertedDate;
 };
