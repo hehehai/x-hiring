ebca0cd08aeb005833c4c4849b36313b672f6807
hehehai <riverhohai@gmail.com>
2024-03-31 19:47:14 +0800

    fix: monorepo prisma in next error

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,3 +1,5 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -6,6 +8,13 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   logging: {
     fetches: {
       fullUrl: true,
