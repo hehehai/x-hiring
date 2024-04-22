d55063e8f737e6d7f2544a150182b9f38f0fa85f
yliu <yangboses@gmail.com>
2024-04-22 22:43:17 +0800

    style: 添加抓取信息来源

--- apps/www/src/app/_components/header.tsx
+++ apps/www/src/app/_components/header.tsx
@@ -28,7 +28,9 @@ export const Header = () => {
         <li>
           🥳 本网站数据来源于
           <HeaderLink link="https://www.v2ex.com/go/jobs">V2EX</HeaderLink>和
-          <HeaderLink link="https://eleduck.com">电鸭社区</HeaderLink>，
+          <HeaderLink link="https://eleduck.com">电鸭社区</HeaderLink>以及
+          <HeaderLink link="https://github.com/ruanyf/weekly/issues/4204">谁在招人</HeaderLink>
+          ，
           对招聘信息使用
           <HeaderLink link="https://makersuite.google.com/?hl=zh-cn">
             Google Gemini
